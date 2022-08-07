5275a197207218f8efdfce9fbcfce0280fa4af7f
Matiboux <matiboux@gmail.com>
2022-08-07 05:26:03 +0200

    Refactor _instantCommitStates

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,10 @@
 import * as path from 'path'
 import * as vscode from 'vscode'
 
-import { API, GitExtension } from './api/git'
+import { API as GitAPI, Change as GitChange, GitExtension, Repository as GitRepository } from './api/git'
 
-function getGitExtension(): API | undefined
+
+function getGitExtension(): GitAPI | undefined
 {
 	const vscodeGit = vscode.extensions.getExtension<GitExtension>("vscode.git")
 	const gitExtension = vscodeGit && vscodeGit.exports
@@ -15,16 +16,10 @@ function getGitExtension(): API | undefined
 		return
 	}
 
-	if (git.repositories.length <= 0)
-	{
-		vscode.window.showErrorMessage('No Git repository found.')
-		return
-	}
-
 	return git
 }
 
-async function _instantCommitStates(...resourceStates: vscode.SourceControlResourceState[])
+function getGitRepository(): GitRepository | undefined
 {
 	const git = getGitExtension()
 	if (!git)
@@ -32,25 +27,129 @@ async function _instantCommitStates(...resourceStates: vscode.SourceControlResou
 		return
 	}
 
-	// const selectedRepo = git.repositories.find(repository => {
-	// 	const uri = sourceControl._rootUri;
-	// 	if (!uri) {
-	// 	console.warn("_rootUri not set");
-	// 	}
-	// 	return repository.rootUri.path === uri.path;
-	// });
+	if (git.repositories.length <= 0)
+	{
+		vscode.window.showErrorMessage('No Git repository found.')
+		return
+	}
 
-	// git.repositories.forEach((repository, i) => {
-	// 	console.log(i, repository.rootUri.path)
-	// })
+	return git.repositories[0]
+}
 
+type ChangeLetter = 'A' | 'M' | 'R' | 'D'
+
+type FileChange =
+	{
+		path: string,
+		relativePath: string,
+		letter: ChangeLetter,
+		change: GitChange | undefined
+	}
+
+
+function generateMessage(fileChanges: FileChange[])
+{
+	let lines = []
+	let commonPath = fileChanges.length >= 1 ? fileChanges[0].relativePath : undefined
+	let commonLetter = fileChanges.length >= 1 ? fileChanges[0].letter : undefined
+
+	const findLongestPathPrefix = (a: string, b: string) =>
+		{
+			while (a.length > 0 && b.length > 0)
+			{
+				if (a === './' || b === './')
+				{
+					return undefined
+				}
+
+				if (a === b)
+				{
+					return a
+				}
+
+				if (a.length > b.length)
+				{
+					a = `${path.dirname(a)}/`
+				}
+				else // b.length > a.length
+				{
+					b = `${path.dirname(b)}/`
+				}
+			}
+
+			return undefined
+		}
+
+	const letterToString = (letter: ChangeLetter | string | undefined) =>
+		{
+			switch (letter)
+			{
+				case "A": return "Add"
+				case "M": return "Update"
+				case "R": return "Rename"
+				case "D": return "Delete"
+				default: return "Update"
+			}
+		}
+
+	for (const fileChange of fileChanges)
+	{
+		// Update common path
+		if (commonPath && commonPath !== fileChange.path)
+		{
+			commonPath = findLongestPathPrefix(commonPath, fileChange.relativePath)
+		}
+
+		// Update common letter
+		if (commonLetter && commonLetter !== fileChange.letter)
+		{
+			commonLetter = undefined
+		}
+
+		lines.push(`${letterToString(fileChange.letter)} ${fileChange.relativePath}`)
+	}
+
+	if (fileChanges.length > 1)
+	{
+		let summary = `${letterToString(commonLetter)} ${fileChanges.length} files`
+
+		if (commonPath)
+		{
+			summary += ` from ${commonPath}`
+		}
+
+		// Prepend summary to lines
+		// Add an empty line to separate commit title from commit message
+		lines.splice(0, 0, summary, '')
+	}
+
+	return lines.join('\n')
+}
+
+async function instantCommit(repository: GitRepository, fileChanges: FileChange[])
+{
+	const commitMessage = generateMessage(fileChanges)
+	console.log(`New message:\n${commitMessage}`)
+
+	await repository.add(fileChanges.map(fileChange => fileChange.path))
+	await repository.commit(commitMessage)
+
+	vscode.window.showInformationMessage(`Instant committed: ${commitMessage.split('\n', 1)[0]}`)
+}
+
+async function _instantCommitStates(...resourceStates: vscode.SourceControlResourceState[])
+{
 	// Todo: Support multiple repositories
-	const repository = git.repositories[0];
+	const repository = getGitRepository()
+	if (!repository)
+	{
+		return
+	}
 
 	const stagedFiles = await repository.diffIndexWithHEAD()
 	const changedFiles = await repository.diffWithHEAD()
 
-	const fileChanges = []
+	const fileChanges: FileChange[] = []
 	for (const resourceState of resourceStates)
 	{
 		const fullPath = resourceState.resourceUri.path
@@ -101,92 +200,7 @@ async function _instantCommitStates(...resourceStates: vscode.SourceControlResou
 		return
 	}
 
-	const generateMessage = (fileChanges: any) =>
-		{
-			let lines = []
-			let commonPath = fileChanges.length >= 1 ? fileChanges[0].relativePath : ''
-			let commonLetter = fileChanges.length >= 1 ? fileChanges[0].letter : ''
-
-			const findLongestPathPrefix = (a: string, b: string) =>
-				{
-					while (a.length > 0 && b.length > 0)
-					{
-						if (a === './' || b === './')
-						{
-							return undefined
-						}
-
-						if (a === b)
-						{
-							return a
-						}
-
-						if (a.length > b.length)
-						{
-							a = `${path.dirname(a)}/`
-						}
-						else // b.length > a.length
-						{
-							b = `${path.dirname(b)}/`
-						}
-					}
-
-					return undefined
-				}
-
-			const letterToString = (letter: string) =>
-				{
-					switch (letter)
-					{
-						case "A": return "Add"
-						case "M": return "Update"
-						case "R": return "Rename"
-						case "D": return "Delete"
-						default: return "Update"
-					}
-				}
-
-			for (const fileChange of fileChanges)
-			{
-				// Update common path
-				if (commonPath && commonPath !== fileChange.path)
-				{
-					commonPath = findLongestPathPrefix(commonPath, fileChange.relativePath)
-				}
-
-				// Update common letter
-				if (commonLetter && commonLetter !== fileChange.letter)
-				{
-					commonLetter = undefined
-				}
-
-				lines.push(`${letterToString(fileChange.letter)} ${fileChange.relativePath}`)
-			}
-
-			if (fileChanges.length > 1)
-			{
-				let summary = `${letterToString(commonLetter)} ${fileChanges.length} files`
-
-				if (commonPath)
-				{
-					summary += ` from ${commonPath}`
-				}
-
-				// Prepend summary to lines
-				// Add an empty line to separate commit title from commit message
-				lines.splice(0, 0, summary, '')
-			}
-
-			return lines.join('\n')
-		}
-
-	const commitMessage = generateMessage(fileChanges)
-	console.log(`New message:\n${commitMessage}`)
-
-	await repository.add(fileChanges.map(fileChange => fileChange.path))
-	await repository.commit(commitMessage)
-
-	vscode.window.showInformationMessage(`Instant committed: ${commitMessage.split('\n', 1)[0]}`)
+	instantCommit(repository, fileChanges)
 }
 
 export function activate(context: vscode.ExtensionContext)
